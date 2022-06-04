2ee278a1f41af272cd58095cf67b18aba2f5e47e
longpshorn <patrick.j.smith1@gmail.com>
2022-06-04 12:53:55 -0500

    Adding 3d grid.

---
+++ MatplotlibCS/Grid3d.cs
@@ -0,0 +1,11 @@
+using Newtonsoft.Json;
+
+namespace MatplotlibCS
+{
+    /// <summary>
+    /// Class describes a grid settings on a plot
+    /// </summary>
+    public class Grid3d : Grid
+    {
+    }
+}
